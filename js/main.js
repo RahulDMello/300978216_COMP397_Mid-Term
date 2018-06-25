fded88bd0a486a1d75098ffec85ce76cc2de94c1
Rahul DMello <rvdmello96@gmail.com>
2018-06-25 09:44:26 -0400

    added internal documentation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 // handler for body.onload
 function init() {
-	var stage;
-	var queue;
-	var dice1;
-	var dice2;
-	var txtDice1;
-	var txtDice2;
+	var stage;  // main createjs stage
+	var queue;  // createjs preload queue
+	var dice1;  // bitmap for the random dice 1
+	var dice2;  // bitmap for the random dice 2
+	var txtDice1;  // text for the random dice 1
+	var txtDice2;  // text for the random dice 2
+	// list of required images with id to be loaded by the queue
 	var assetData = [
 		{id: "dice1", src: "/Assets/images/1.png"},
 		{id: "dice2", src: "/Assets/images/2.png"},
@@ -15,20 +16,24 @@ function init() {
 		{id: "dice6", src: "/Assets/images/6.png"},
 		{id: "start", src: "/Assets/images/StartButton.png"}
 	]
+	
 	preload();
 	
+	// preloads all the needed assets. on complete calls begin
 	function preload() {
 		queue = new createjs.LoadQueue();
 		queue.on("complete", begin, this);
 		queue.loadManifest(assetData);
 	}
 	
+	// game starting handler. sets up the stage and ticker.
 	function begin() {
 		stage = new createjs.Stage(document.getElementById("canvas"));
 		createjs.Ticker.setFPS(60);
-		// create an event listener to count off frames
+		// create an event listener (update) to count off frames
 		createjs.Ticker.on("tick", update, this);
 		
+		// setting up the start button and adding to the stage
 		var imgStart = queue.getResult("start");
 		var btnStart = new createjs.Bitmap(imgStart);
 		btnStart.addEventListener("click", showNewRoll);
@@ -37,22 +42,29 @@ function init() {
 		stage.addChild(btnStart);
 	}
 	
+	//update the roll
 	function showNewRoll() {
+		// remove previous children other then start button
 		stage.removeChild(dice1);
 		stage.removeChild(dice2);
 		stage.removeChild(txtDice1);
 		stage.removeChild(txtDice2);
+		
+		// choose the random dices
 		var txt1 = (Math.floor(Math.random() * 6) + 1);
 		var txt2 = (Math.floor(Math.random() * 6) + 1);
 		var img1 = queue.getResult("dice" + txt1);
 		var img2 = queue.getResult("dice" + txt2);
+		
+		// setup the dices
 		dice1 = new createjs.Bitmap(img1);
 		dice2 = new createjs.Bitmap(img2);
 		dice1.x = 640 - (dice1.getBounds().width * 2) - 50;
 		dice2.x = 640 - (dice1.getBounds().width) - 30;
 		dice1.y = 100;
 		dice2.y = 100;
-		
+		 
+		// setup the text for the dices
 		txtDice1 = new createjs.Text(txt1, "20px console", "#000");
 		txtDice2 = new createjs.Text(txt2, "20px console", "#000");
 		txtDice1.x = dice1.x + (dice1.getBounds().width * 0.5);
@@ -60,12 +72,14 @@ function init() {
 		txtDice1.y = 100 + dice1.getBounds().height + 20;
 		txtDice2.y = 100 + dice2.getBounds().height + 20;
 		
+		// add the text and dice to stage
 		stage.addChild(dice1);
 		stage.addChild(dice2);
 		stage.addChild(txtDice1);
 		stage.addChild(txtDice2);
 	}
 	
+	// ticker handler
 	function update() {
 		stage.update();
 	}
